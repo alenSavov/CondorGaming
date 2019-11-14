ab719c9c52a936da1b74967ccfbddfdf86216a30
alenSavov <alensavov@gmail.com>
2019-11-14 23:03:11 +0200

    Added all files

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'sass',
+                    src: ['*.scss'],
+                    dest: '../CondorGaming/css',
+                    ext: '.css'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', ['sass']);
+
+};
+
